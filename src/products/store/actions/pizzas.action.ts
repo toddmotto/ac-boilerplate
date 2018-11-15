ebc677a15668a2cdac714e9fb23a59bbb434de65
Szymon Piwowarczyk <szymon.piwowarczyk@s3group.com>
2018-11-15 19:25:26 +0100

    Remove unneeded action from pizza store

--- src/products/store/actions/pizzas.action.ts
+++ src/products/store/actions/pizzas.action.ts
@@ -2,15 +2,9 @@ import { Action } from '@ngrx/store';
 
 import { Pizza } from '../../models/pizza.model';
 
-export const MY_ACTION = '[Feature] My Action';
 export const LOAD_PIZZAS = '[Pizzas] LoadPizzas';
 export const LOAD_PIZZAS_SUCCESS = '[Pizzas] LoadPizzasSuccess';
 export const LOAD_PIZZAS_FAIL = '[Pizzas] LoadPizzasFail';
-
-export class MyAction implements Action {
-  readonly type = MY_ACTION;
-  constructor(public payload: any) {}
-}
 
 export class LoadPizzas implements Action {
   readonly type = LOAD_PIZZAS;
@@ -28,7 +22,6 @@ export class LoadPizzasFail implements Action {
 
 // action types
 export type PizzasAction =
-  MyAction |
   LoadPizzas |
   LoadPizzasSuccess |
   LoadPizzasFail;
